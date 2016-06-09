5f1a52fc2f0dbc8b49ac051b9ee9295ba3e96ae0
N.Shimizu <chikoski@gmail.com>
2016-06-09 09:13:04 +0900

    added auto text feature

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
 const gulp = require("gulp");
 
-const files = ["id", "oddnumber"];
+const preamble = "spec/preamble.js";
+
+
+const files = ["01", "02", "03", "04", "05",
+               "06", "07", "08", "09", "10"];
+
+//const files = ["01", "02", "03", "04", "05"];
 
 gulp.task("test", ["concat", "mocha"]);
 
@@ -13,12 +19,12 @@ gulp.task("mocha", () => {
 gulp.task("concat", () => {
   const concat = require('gulp-concat');
   return files.map(id => {
-    const js = `./js/${id}.js`;
+    const js = `./${id}/${id}.js`;
     const spec = `./spec/${id}_spec.js`;
     const test = `${id}_test.js`;
     console.log(`concat ${js} and ${spec}`);
 
-    return gulp.src([js, spec])
+    return gulp.src([preamble, js, spec])
       .pipe(concat(test))
       .pipe(gulp.dest("./test/"));
   });
